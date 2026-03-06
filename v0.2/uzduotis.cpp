bef523ffedfd041ad46d471157b372fee15f21bb
thrust0 <dragforce@proton.me>
2026-03-06 09:32:00 +0200

    commented the time tester and added try catch to main

--- v0.2/uzduotis.cpp
+++ v0.2/uzduotis.cpp
@@ -5,8 +5,8 @@
 
 
 int main() 
-{
-    auto file_read_time = std::chrono::milliseconds(0);
+try{
+    //auto file_read_time = std::chrono::milliseconds(0);
     srand(time(0));
     for(int i = 0; i<6; i++) std::cout << std::endl;
     std::vector<Students>group;
@@ -63,8 +63,8 @@ int main()
                 break;
             }
             break;
-        auto end_input = std::chrono::high_resolution_clock::now();
-        file_read_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_input - start_input);
+        //auto end_input = std::chrono::high_resolution_clock::now();
+        //file_read_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_input - start_input);
         }
         else if(menu_option == 5)
         {
@@ -86,35 +86,39 @@ int main()
     std::cout << "Pasirinkite kur norite, kad duomenys būtu išvesti:\n\t'1'Terminale\n\t'2'Teksto faile\nĮveskite pasirinkimą: ";
     output_option = get_int(1,2);
     
-    auto start = std::chrono::high_resolution_clock::now(); //timing the start
+    //auto start = std::chrono::high_resolution_clock::now(); //timing the start
     sort_output(group, sort_option);
     if(output_option == 1 && menu_option == 4)
     {
         temp_output(group);
-        auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-        std::cout << "Laiko uztruko: " << duration.count()+file_read_time.count() << " milisekundes" << std::endl;
+        //auto end = std::chrono::high_resolution_clock::now();
+        //auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        //std::cout << "Laiko uztruko: " << duration.count()+file_read_time.count() << " milisekundes" << std::endl;
         return 0;
     }
     else if(output_option == 1)
     {
         output(group);
-        auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-        std::cout << "Laiko uztruko: " << duration.count()+file_read_time.count() << " milisekundess" << std::endl;
+        //auto end = std::chrono::high_resolution_clock::now();
+        //auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        //std::cout << "Laiko uztruko: " << duration.count()+file_read_time.count() << " milisekundess" << std::endl;
         return 0;
     }
     else
     {
         file_output(group);
-        auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-        std::cout << "Laiko uztruko: " << duration.count()+file_read_time.count() << " milisekundess" << std::endl;
-        std::cout << "Rezultatai išvesti teksto faile." << std::endl;
+        //auto end = std::chrono::high_resolution_clock::now();
+        //auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        //std::cout << "Laiko uztruko: " << duration.count()+file_read_time.count() << " milisekundess" << std::endl;
+        //std::cout << "Rezultatai išvesti teksto faile." << std::endl;
 
         return 0;
     }
 
+}
+catch(const std::exception& e)
+{
+    std::cerr << "Klaida: " << e.what() << std::endl;
 }
 
 // Interactive input: read name, grades and exam. ';' as name exits.
