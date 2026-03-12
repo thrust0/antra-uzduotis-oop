709bcb7e0f95ae026a74f9613d2a34df5eefbcfe
thrust0 <dragforce@proton.me>
2026-03-12 14:43:34 +0200

    isnaujo prideta 2 testavimo nuotraukos

--- test.cpp
+++ test.cpp
@@ -4,12 +4,16 @@ int main()
 try{
     std::cout << std::endl << "1k: " << std::endl; 
     test_data_processing("studentInput/studentai_gen1000.txt");
+
     std::cout << std::endl <<  "10k: " << std::endl; 
     test_data_processing("studentInput/studentai_gen10000.txt");
+
     std::cout << std::endl <<  "100k: " << std::endl; 
     test_data_processing("studentInput/studentai_gen100000.txt");
+
     std::cout << std::endl <<  "1mil: " << std::endl; 
     test_data_processing("studentInput/studentai_gen1000000.txt");
+    
     std::cout << std::endl <<  "10mil: " << std::endl; 
     test_data_processing("studentInput/studentai_gen10000000.txt");
     return 0;
